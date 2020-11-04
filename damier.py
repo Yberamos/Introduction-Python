0224642eb8b06773aaab52c16b51319725b32a59
Yberamos <gregoiredemoulin2@gmail.com>
2020-11-04 08:54:09 +0100

    Ajout boutton quitter + agrandis canvas

--- damier.py
+++ damier.py
@@ -6,11 +6,12 @@ from random import randrange
 def damier():
     "dessine un damier"
     i, j = 0, 0
+    global size_carré
 
     while j < 10:
         while i < 10:
             if (i+j) % 2 != 1:
-                can.create_rectangle(i*20, j*20, (i+1)*20, (j+1)*20, outline='black', fill='black')
+                can.create_rectangle(i*size_carré, j*size_carré, (i+1)*size_carré, (j+1)*size_carré, outline='black', fill='navy')
             i += 1
         j += 1
         i = 0
@@ -18,23 +19,21 @@ def damier():
 
 def pion():
     "dessine un pion aléatoire"
-
+    global size_carré
     position = randrange(0, 99)
     i = position % 10
     j = position // 10
 
-    can.create_oval(i*20, j*20, (i+1)*20, (j+1)*20, fill='red')
+    can.create_oval((i*size_carré)+4, (j*size_carré)+4, ((i+1)*size_carré)-4, ((j+1)*size_carré)-4, fill='red')
 
-
+size_carré = 20
 fen = Tk()
 
-can = Canvas(fen, width=200, height=200, bg='ivory')
+can = Canvas(fen, width=size_carré*10, height=size_carré*10, bg='ivory')
 can.pack(side=TOP, padx=5, pady=5)
 
-b1 = Button(fen, text='damier', command=damier)
-b1.pack(side=LEFT, padx=3, pady=3)
-
-b2 = Button(fen, text='pion', command=pion)
-b2.pack(side=RIGHT, padx=3, pady=3)
+Button(fen, text='damier', command=damier).pack(side=LEFT, padx=3, pady=3)
+Button(fen, text='pion', command=pion).pack(side=BOTTOM, padx=3, pady=3)
+Button(fen, text='Quitter', command=fen.destroy).pack(side=RIGHT, padx=3, pady=3)
 
 fen.mainloop()
